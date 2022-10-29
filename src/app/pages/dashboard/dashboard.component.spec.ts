c626becc33817989d5ab2464410834dab05e1f90
Mukhsin Fahmi <mukhsin.fahmi@gmail.com>
2022-10-29 10:25:04 +0700

    Fix unit test

--- src/app/pages/dashboard/dashboard.component.spec.ts
+++ src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { NgxEchartsModule } from "ngx-echarts"
 
 import { DashboardComponent } from "./dashboard.component"
+import { SharedModule } from "../../shared/shared.module"
 
 describe("DashboardComponent", () => {
   let component: DashboardComponent
@@ -9,6 +11,13 @@ describe("DashboardComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
+      imports: [
+        SharedModule,
+        NgxEchartsModule.forRoot({
+          // echarts
+          echarts: () => import("echarts"),
+        }),
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(DashboardComponent)
