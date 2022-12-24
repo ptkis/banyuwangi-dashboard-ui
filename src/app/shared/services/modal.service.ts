093b47500fb0405647f006eeb8e50564cf43a48b
Mukhsin Fahmi <mukhsin.fahmi@gmail.com>
2022-12-24 12:52:41 +0700

    Create custom toast

--- src/app/shared/services/modal.service.ts
+++ src/app/shared/services/modal.service.ts
@@ -1,12 +1,27 @@
 import { Dialog } from "@angular/cdk/dialog"
-import { Injectable } from "@angular/core"
+import { forwardRef, Inject, Injectable, Optional } from "@angular/core"
+import { IndividualConfig, ToastrService } from "ngx-toastr"
 import { ModalComponent } from "../components/modal/modal.component"
+import { NotificationToastComponent } from "../components/notification-toast/notification-toast.component"
+
+export interface INotificationToastData {
+  message: string
+  title?: string
+  image_logo?: string
+  image_main?: string
+  data: any
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class ModalService {
-  constructor(public dialog: Dialog) {}
+  constructor(
+    public dialog: Dialog,
+    @Optional()
+    @Inject(forwardRef(() => ToastrService))
+    private toastr: ToastrService
+  ) {}
 
   showLoader(title?: string) {
     return this.dialog.open<boolean>(ModalComponent, {
@@ -36,5 +51,17 @@ export class ModalService {
       disableClose: true,
       width: "300px",
     })
+  }
+
+  showNotificationToast(
+    data: INotificationToastData,
+    toastrConfig: Partial<IndividualConfig>
+  ) {
+    const toast = this.toastr.show(data.message, data.title, {
+      ...toastrConfig,
+      toastComponent: NotificationToastComponent,
+    })
+    toast.toastRef.componentInstance.data = data
+    return toast
   }
 }
