095fc17146230428aee52b15706de700ae06aa9f
Mukhsin Fahmi <mukhsin.fahmi@gmail.com>
2023-11-09 10:24:39 +0700

    Alert service

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
-import { Subject } from "rxjs"
+import { BehaviorSubject, Subject } from "rxjs"
 import { environment } from "src/environments/environment"
 import { ListResponse } from "./pages/dialog/cctvlist/cctvlist.service"
 import { INotificationToastData } from "./shared/services/modal.service"
@@ -10,6 +10,9 @@ import { INotificationToastData } from "./shared/services/modal.service"
 })
 export class AppService {
   notificationSubject$ = new Subject<INotificationToastData>()
+  cctvIdHasAlerts$ = new BehaviorSubject<string[]>([])
+
+  idHasAlerts: string[] = []
 
   constructor(private http: HttpClient) {}
 
@@ -18,5 +21,16 @@ export class AppService {
       `${environment.serverBaseUrl}/v1/fcm/device/token/${registrationToken}`,
       {}
     )
+  }
+
+  addAlertToId(id: string) {
+    this.idHasAlerts = [...this.idHasAlerts, id]
+
+    this.cctvIdHasAlerts$.next(this.idHasAlerts)
+  }
+
+  removeAlertFromId(id: string) {
+    this.idHasAlerts = this.idHasAlerts.filter((id2) => id2 !== id)
+    this.cctvIdHasAlerts$.next(this.idHasAlerts)
   }
 }
