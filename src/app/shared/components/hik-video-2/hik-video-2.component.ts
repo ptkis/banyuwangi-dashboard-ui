3063cfaef95d660906fe8e845e01453a65bdfd4a
Thomas Edwin Santosa <thomas@katalisindonesia.com>
2024-04-18 20:15:23 +0700

    Revert "disable live check"
    This reverts commit 31eb9f99bb6c6c33abefc466a9e8b7874b24e841.

--- src/app/shared/components/hik-video-2/hik-video-2.component.ts
+++ src/app/shared/components/hik-video-2/hik-video-2.component.ts
@@ -120,14 +120,12 @@ export class HikVideo2Component implements AfterViewInit {
     untilDestroyed(this)
   )
 
-  /*
   liveCheckTimer$ = timer(0, environment.liveCheckInterval * 1000).pipe(
     tap(() => {
       this.checkLive()
     }),
     untilDestroyed(this)
   )
-*/
 
   isDebug = false
 
@@ -154,7 +152,7 @@ export class HikVideo2Component implements AfterViewInit {
       this.isDebug = true
     }
     // create a timer to check if the player is live
-    // this.liveCheckTimer$.subscribe()
+    this.liveCheckTimer$.subscribe()
   }
 
   reloadPlayer() {
