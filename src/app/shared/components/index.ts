300cfbd52d873dce570774efc60d701090fe8793
Thomas Edwin Santosa <thomas@katalisindonesia.com>
2024-04-22 03:34:58 +0700

    disable export hik 2

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,14 +1,12 @@
 import { DateFormatPipe } from "../pipes/date-format.pipe"
 import { ChartPanelComponent } from "./chart-panel/chart-panel.component"
 import { HikVideoComponent } from "./hik-video/hik-video.component"
-import { HikVideo2Component } from "./hik-video-2/hik-video-2.component"
 
 import { NotificationToastComponent } from "./notification-toast/notification-toast.component"
 
 export const sharedComponents = [
   ChartPanelComponent,
   HikVideoComponent,
-  HikVideo2Component,
   NotificationToastComponent,
 
   DateFormatPipe,
